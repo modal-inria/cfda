65a860ae8feea440395ca5d234f1644ac38afaed
Quentin Grimonprez <quentin.grimonprez@inria.fr>
2019-10-22 13:28:47 +0200

    add test for compute_time_spent

--- tests/testthat/test.stat.R
+++ tests/testthat/test.stat.R
@@ -20,3 +20,16 @@ test_that("compute_Time_Spent_intern works with a higher K", {
   expect_equal(sum(out), max(dat$time))
   expect_equal(out, expectedOut)
 })
+
+test_that("compute_Time_Spent works", {
+  dat <- data.frame(id = rep(1:2, c(5, 3)), time = c(0, 1.5, 4, 5, 6, 0, 3, 6), state = c(1, 3, 2, 1, 1, 1, 2, 3))
+  
+  out <- compute_Time_Spent(dat, K = 3)
+  expectedOut <- rbind(c(1.5 + 1, 1, 2.5),
+                       c(3, 3, 0))
+  colnames(expectedOut) = 1:3
+  rownames(expectedOut) = 1:2
+  
+  expect_equal(out, expectedOut)
+})
+
