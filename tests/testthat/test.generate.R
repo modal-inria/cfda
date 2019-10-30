cf43a0f0b2fee0c83c52b3c271c294a17cc4d160
Quentin Grimonprez <quentin.grimonprez@inria.fr>
2019-10-30 16:15:47 +0100

    add test for generate_2State

--- tests/testthat/test.generate.R
+++ tests/testthat/test.generate.R
@@ -1,7 +1,7 @@
 context("Data generation")
 
 
-test_that("check generate_Markov_cfd output has the right format",{
+test_that("generate_Markov_cfd output has the right format", {
   n <- 10
   K <- 4
   Tmax <- 10
@@ -23,7 +23,7 @@ test_that("check generate_Markov_cfd output has the right format",{
   # state are betwwen 1 and K
   expect_equal(sort(unique(d_JK$state)), 1:K)
   
-  # all time are betwwen 0 and Tmax
+  # all time are between 0 and Tmax
   expect_true(all(d_JK$time <= Tmax))
   expect_true(all(d_JK$time >= 0))
   
@@ -32,4 +32,40 @@ test_that("check generate_Markov_cfd output has the right format",{
   
   # check time values are ordered per trajectory
   expect_true(all(tapply(d_JK$time, d_JK$id, function(x) {all(order(x) == seq_along(x))})))
+})
+
+
+test_that("generate_2State output has the right format", {
+  n <- 10
+
+  d <- generate_2State(n)
+  
+  ## format
+  expect_s3_class(d, "data.frame")
+  expect_equal(ncol(d), 3)
+  expect_equal(colnames(d), c("id", "time", "state"))
+  
+  
+  ## content
+  # id are between 1 and n
+  expect_equal(sort(unique(d$id)), 1:n)
+  
+  # state are betwwen 1 and 2
+  expect_equal(sort(unique(d$state)), 1:2)
+  
+  # all time are between 0 and 1
+  expect_true(all(d$time <= 1))
+  expect_true(all(d$time >= 0))
+  
+  # each trajectory must start by a 0, so there must have n 0
+  expect_equal(sum(d$time == 0), n)
+  
+  # check time values are ordered per trajectory
+  expect_true(all(tapply(d$time, d$id, function(x) {all(order(x) == seq_along(x))})))
+  
+  # check state values are ordered per trajectory
+  expect_true(all(tapply(d$state, d$id, function(x) {all(order(x) == seq_along(x))})))
+  
+  # each individuals must have records
+  expect_equivalent(as.numeric(table(d$id)), rep(2, n))
 })
