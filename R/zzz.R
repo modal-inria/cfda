c1d7b1c83d60da7e1e4bc26544b7ec56890e8d58
Quentin Grimonprez <qgrimonprez@diagrams-technologies.com>
2024-11-20 17:01:11 +0100

    feat: update startup messages

--- R/zzz.R
+++ R/zzz.R
@@ -3,7 +3,8 @@
   packageStartupMessage(
     paste(
       "INFO: Since cfda 0.11, the computing method of compute_optimal_encoding function has changed.",
-      "To use the former one, add the argument: method='parallel'"
+      "To use the former one, add the argument: method='parallel'",
+      "\nINFO: Since cfda 0.12, the used covariance estimator is the biaised one"
     )
   )
 }
